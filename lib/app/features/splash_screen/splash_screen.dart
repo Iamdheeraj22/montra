9b1d1b17b19b2e3a7361c5dd3e5639af7fa9c5e7
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-09-06 05:19:37 +0530

    feat : design UI of splash_screen

---
+++ lib/app/features/splash_screen/splash_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:montra/app/core/res/app_colors.dart';
+import 'package:montra/app/features/on_boarding/on_boarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 2), () {
+        if (mounted) {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (_) => const OnBoardingScreen()));
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              height: 47,
+              width: 50,
+              constraints: const BoxConstraints(
+                maxHeight: 47,
+                maxWidth: 50,
+              ),
+              margin: const EdgeInsets.only(top: 15, left: 35),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFFEF5DA8).withOpacity(0.5),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFFEF5DA8).withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Montra',
+              style: TextStyle(
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 3.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
