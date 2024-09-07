78d102fe844e0bdbc73bf7ad36e003fb23cb7a8b
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-09-07 14:02:24 +0530

    feat : Handle gestures of tapping on screen and initialise navigation in Material App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:montra/app/core/routes/navigation_routes.dart';
 
 import 'app/features/splash_screen/splash_screen.dart';
 
@@ -12,20 +13,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Montra',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      themeMode: ThemeMode.light,
-      home: const SplashScreen(),
-      builder: (context, child) {
-        return Scaffold(
-          body: child,
-        );
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild!.unfocus();
+        }
       },
+      child: MaterialApp(
+        title: 'Montra',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        themeMode: ThemeMode.light,
+        home: const SplashScreen(),
+        onGenerateRoute: NavigationRoutes.onGenerateRoute,
+        builder: (context, child) {
+          return Scaffold(
+            body: child,
+          );
+        },
+      ),
     );
   }
 }
