6438f3386f47bf1322d0b8f894d492d3dbb2b4f8
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-09-08 21:08:04 +0530

    feat : create separate button for google

---
+++ lib/app/reusable_widgets/google_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:montra/app/core/res/app_colors.dart';
+import 'package:montra/app/core/res/app_images.dart';
+
+class GoogleButton extends StatelessWidget {
+  const GoogleButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+  final String title;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        constraints: const BoxConstraints(
+          maxWidth: double.infinity,
+          maxHeight: 50,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              AppImages.svgGoogle,
+              height: 32,
+              width: 32,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              title,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.dark,
+                  fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
