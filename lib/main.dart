378cebd6d019eec81253a2d8c51b9f351d0dbd4b
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-09-14 23:33:03 +0530

    feat : create global user bloc for entire application and assign to main material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:montra/app/core/routes/navigation_routes.dart';
+import 'package:montra/app/core/user_state/user_state_bloc.dart';
 import 'package:montra/firebase_options.dart';
 
 import 'app/features/splash_screen/splash_screen.dart';
@@ -27,21 +29,24 @@ class MyApp extends StatelessWidget {
           currentFocus.focusedChild!.unfocus();
         }
       },
-      child: MaterialApp(
-        title: 'Montra',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      child: BlocProvider(
+        create: (context) => UserStateBloc(),
+        child: MaterialApp(
+          title: 'Montra',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.light,
+          home: const SplashScreen(),
+          onGenerateRoute: NavigationRoutes.onGenerateRoute,
+          builder: (context, child) {
+            return Scaffold(
+              body: child,
+            );
+          },
         ),
-        themeMode: ThemeMode.light,
-        home: const SplashScreen(),
-        onGenerateRoute: NavigationRoutes.onGenerateRoute,
-        builder: (context, child) {
-          return Scaffold(
-            body: child,
-          );
-        },
       ),
     );
   }
